ae1b5b45e2ee06da1c5e8e538a541df7dd30890e
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-06 19:21:12 -0800

    initial commit

---
+++ Loops_1/10.rb
@@ -0,0 +1,14 @@
+# Your friends just showed up! Given the following array of names, 
+# use a for loop to greet each friend individually.
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for i in 1..1
+  friends.each do |name|
+    puts "Hello, #{name}!"
+  end
+
+  
+end
+
+
