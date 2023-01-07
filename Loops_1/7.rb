18ec61ca7634aa776d12ca4c143048d0ba964d6e
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-06 19:02:31 -0800

    initial commit

---
+++ Loops_1/7.rb
@@ -0,0 +1,12 @@
+# The following code outputs a countdown from 10 to 1. Modify the code so 
+#that it counts from 1 to 10 instead.
+
+count = 0
+
+until count == 10
+  
+  count += 1
+  puts count
+  
+end
+
