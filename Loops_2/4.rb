3252b528ae94c7264d4bb78c758ee98b3fc140b0
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-08 15:30:37 -0800

    Get the Sum

---
+++ Loops_2/4.rb
@@ -0,0 +1,11 @@
+loop do
+  puts 'What does 2 + 2 equal?'
+  answer = gets.chomp.to_i
+  if answer == 4
+    puts "Thats Correct!"
+    break
+  else
+    puts "Wrong Answer!Try again."
+  end
+
+end
