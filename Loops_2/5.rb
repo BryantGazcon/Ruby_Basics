de5eef5587e202d653004f3ead78c55da7280e9b
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-08 15:41:29 -0800

    insert numbers

---
+++ Loops_2/5.rb
@@ -0,0 +1,13 @@
+numbers = []
+
+loop do
+  puts 'Enter any number:'
+  input = gets.chomp.to_i
+  numbers.push(input)
+  if numbers.size == 5
+    break
+  end
+
+end
+puts "Your array is full of the numbers #{numbers}"
+
