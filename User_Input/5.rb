09b62d092a2e96506b8818719dea7e8a9cbb4bbe
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-08 17:33:01 -0800

    Launch School Printer(Part 1)

---
+++ User_Input/5.rb
@@ -0,0 +1,22 @@
+
+
+def launchschool_print
+  $amount.times do
+    puts "Launch School is the best!"
+  end
+end
+
+
+loop do
+  puts "How many output lines do you want? Enter a number >= 3:"
+  $amount = gets.chomp.to_i
+
+  if $amount >= 3
+    launchschool_print
+    break
+  elsif $amount < 3
+    puts "Thats not enough lines."
+  end
+end
+
+
