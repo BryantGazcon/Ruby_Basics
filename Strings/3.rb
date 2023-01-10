0010f3004592add8ce29f15de99e18bd4e0c08a0
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-10 14:53:26 -0800

    Ignoring Case

--- Strings/3.rb
+++ Strings/3.rb
@@ -2,3 +2,9 @@ name = 'Roger'
 
 puts name.casecmp?("RoGeR")
 puts name.casecmp?("DAVE")
+
+#when comparing two strings, we dont really care about the case, we care much more
+#about their value.
+#output:
+#true
+#false
