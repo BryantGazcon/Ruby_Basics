453eb04da019618e4441b9e62af279c439236af4
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-10 14:12:29 -0800

    Truth Number

--- Conditionals/5.rb
+++ Conditionals/5.rb
@@ -5,3 +5,7 @@ if number
 else
   puts "I don't have a favorite number."
 end
+
+
+# output is "My favorite number is 7", in ruby every expression evaluates to true
+#unless false or nil is used in condition.
