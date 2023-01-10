a466564e770a5aceff26e36d7b2ec7c8971c367c
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-09 18:40:49 -0800

    B,L,D Part 6

---
+++ Return/6.rb
@@ -0,0 +1,13 @@
+def meal
+  return 'Breakfast'
+  'Dinner'
+  puts 'Dinner'
+end
+
+puts meal
+
+=begin
+The following code will output breakfast becuase the return keyword
+on the first line of the method immediately exits the method and returns
+the provided value.
+=end
