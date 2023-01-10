9bd6bd2ed08e901cd957f14238527e066b4fa0ea
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-10 15:04:53 -0800

    Tricky Formatting

---
+++ Strings/6.rb
@@ -0,0 +1,10 @@
+
+
+state = 'tExAs'
+
+state.capitalize!
+
+puts state
+
+# Destructive version of capitalize is used meaning that it mutates the caller
+# and when invoking state Texas will always be capitalized correctly.
