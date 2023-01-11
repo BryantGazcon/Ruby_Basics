7d9f4609d87646e3114f821e1d5a3a25e3613c7b
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-11 15:45:09 -0800

    Nested Hashes

---
+++ Hashes/9.rb
@@ -0,0 +1,7 @@
+vehicles = {car: {type: 'sedan',color: 'blue',year: 2003 },
+            truck: {type: 'pickup', color: 'red', year: 1998}
+}
+
+p vehicles
+
+p vehicles[:truck][:year] #get some value from the hash
